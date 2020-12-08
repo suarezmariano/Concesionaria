571560810b638058cce8bb1b19647538279119ec
suarezmariano <marianosuarez@hotmail.com>
2020-12-08 14:20:59 -0300

    Creando archivos base

---
+++ app.js
@@ -0,0 +1,16 @@
+const  autos = require ('./autos.js');
+
+const concesionaria = {
+    autos: autos,
+
+
+
+
+
+}
+
+const persona = {
+    nombre: "Juan",
+    capacidadDePagoEnCuotas: 30000,
+    capacidadDePagoTotal: 100000000
+ };
---
+++ autos.js
@@ -0,0 +1,25 @@
+let autos = [
+    {
+       marca:  "Ford",
+       modelo:  "Fiesta",
+       color: "Azul", 
+       anio:  2019,
+       precio: 150000,
+       kms: 0,
+       cuotas: 12,
+       patente: "APL123",
+       vendido: true  
+       },
+        { 
+        marca:  "Toyota",
+       modelo:  "Corolla",
+       color: "Blanco", 
+       anio:  2019,
+      precio: 100000,
+       kms: 0,
+       cuotas: 14,
+       patente: "JJK116",
+       vendido: true
+         },
+  ];
+  module.exports = autos;
